e05606041b95502620f775d96170c7341ba67907
Ben Parisot <me@benparisot.com>
2019-03-01 10:49:23 -0800

    make candidate-list-component.js and put new make candidate list function there, export

---
+++ src/candidate-list-component.js
@@ -0,0 +1,15 @@
+export default function makeCandidateList(candidate) {
+    const html = `
+    <li>
+                <button id="minus-one"> - </button>
+                <section id="candidate-detail">
+                    <img src="${candidate.imageURL}">
+                    <h2>${candidate.lastName}</h2>
+                </section>
+                <button id="plus-one"> + </button>
+            </li>`;
+    
+    const template = document.createElement('template');
+    template.innerHTML = html;
+    return template.content;
+}
--- test/make-candidate-list.tests.js
+++ test/make-candidate-list.tests.js
@@ -1,29 +1,13 @@
+import makeCandidateList from '../src/candidate-list-component.js';
 const test = QUnit.test;
 
 const candidate = {
-    firstName: 'Jay',
-    lastName: 'Inslee',
-    imageURL: './assets/candidates-inslee.jpg',
+    firstName: 'Cory',
+    lastName: 'Booker',
+    imageURL: './assets/candidates-booker.jpg',
     debateScore: 0,
     debateWins: 0
 };
-
-function makeCandidateList(candidate) {
-    const html = `
-    <li>
-                <button id="minus-one"> - </button>
-                <section id="candidate-detail">
-                    <img src="./assets/candidates-booker.jpg">
-                    <h2>Booker</h2>
-                </section>
-                <button id="plus-one"> + </button>
-            </li>`;
-    
-    const template = document.createElement('template');
-    template.innerHTML = html;
-    return template.content;
-}
-
 
 test('create html list of candidates using template and interpolation', assert => {
 
