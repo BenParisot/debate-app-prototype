4ca2bb2c5108d73087d6e10f64e02dca9a9e03eb
Ben Parisot <me@benparisot.com>
2019-03-01 14:13:00 -0800

    call scoreCandidates() on index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import candidates from '../data/candidates.js';
 import loadCandidates from './candidate-list-component.js';
 import startDebate from './debate-start-component.js';
+import scoreCandidates from './score-plus-minus-component.js';
 
 loadCandidates(candidates);
 console.log(candidates);
 startDebate();
+scoreCandidates(candidates);
 
 
