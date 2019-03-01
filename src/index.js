13e54e4279c695c5e7ffd3b50f407eb1be4cab50
Ben Parisot <me@benparisot.com>
2019-03-01 11:44:06 -0800

    subtract 1 from candidate.debateScore when user clicks candidate minus one button

--- src/index.js
+++ src/index.js
@@ -8,9 +8,15 @@ startDebate();
 
 candidates.forEach(candidate => {
     const candidatePlusOneButtonNode = document.getElementById(`${candidate.id}-plus-one`);
+    const candidateMinusOneButtonNode = document.getElementById(`${candidate.id}-minus-one`);
 
     candidatePlusOneButtonNode.addEventListener('click', () => {
         candidate.debateScore++;
+        console.log(candidate);
+    });
+
+    candidateMinusOneButtonNode.addEventListener('click', () => {
+        candidate.debateScore--;
         console.log(candidate);
     });
     
