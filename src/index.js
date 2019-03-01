e3526e7c74efb5cd4479cae662e8d241ae0fea51
Ben Parisot <me@benparisot.com>
2019-03-01 11:10:21 -0800

    create start-debate button and get it to set candidateScore to 0 when clicked

--- src/index.js
+++ src/index.js
@@ -2,5 +2,13 @@ import candidates from '../data/candidates.js';
 import loadCandidates from './candidate-list-component.js';
 
 loadCandidates(candidates);
+console.log(candidates);
 
+const startDebateButtonNode = document.getElementById('start-debate');
 
+startDebateButtonNode.addEventListener('click', () => {
+    candidates.forEach(candidate => {
+        candidate.debateScore = 0;
+    });
+    console.log(candidates);
+});
